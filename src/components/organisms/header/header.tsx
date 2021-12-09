851de0cc52e698690fdf76ceee792e35d794f159
Yana Kanavalik <yana.niaronskaya@ah.nl>
2021-12-08 20:27:32 -0800

    feat: change icons, clean dependencies

--- src/components/organisms/header/header.tsx
+++ src/components/organisms/header/header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "./header.scss";
-import TomatoIcon from "../../../../assets/tomato-icon.svg";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <TomatoIcon className={styles.header__logo} />
+      <img
+        className={styles.header__logo}
+        src={require("./../../../../assets/icons/tomato-icon.png")}
+        alt="Tomato icon"
+      />
       <span className={styles.header__title}>Pomodoro</span>
     </header>
   );
